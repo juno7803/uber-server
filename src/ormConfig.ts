64dbf6f591ad168a7f7b73463c261b0958a2f7ff
junolee <juno7803@naver.com>
2020-07-11 23:23:57 +0900

    install postgre

---
+++ src/ormConfig.ts
@@ -0,0 +1,14 @@
+import { ConnectionOptions } from "typeorm";
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
+  database: "nuber",
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"], // 여기 안에 모델 파일들을 저장
+  host: process.env.DB_ENDPOINT || "localhost",
+  port: 5432,
+  username: process.env.DB_USERNAME || "junolee",
+  password: process.env.DB_PASSWORD || "wlqgkq7803"
+};
+
+export default connectionOptions;
