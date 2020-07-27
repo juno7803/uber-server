ccd635c3140bf413f9c0af0af644b4045a22489d
junolee <juno7803@naver.com>
2020-07-27 11:57:08 +0900

    #2.34 StartPhoneVerification Resolver (1/3) + twilio 설치

---
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -0,0 +1,30 @@
+import { Resolvers } from "../../../types/resolvers";
+import { StartPhoneVerificationMutationArgs, StartPhonVerificationResponse } from "src/types/graph";
+import Verification from "../../../entities/Verification"
+
+const resolvers: Resolvers = {
+  Mutation: {
+    StartPhoneVerification: async (
+      _,
+      args: StartPhoneVerificationMutationArgs
+    ): Promise<StartPhonVerificationResponse> => {
+        const {phoneNumber} = args;
+        try{
+            const existingVerification = await Verification.findOne({
+                payload: phoneNumber
+            })
+            if(existingVerification){
+                existingVerification.remove();
+            }
+        }catch(error){
+            return{
+                ok: false,
+                error: error.message
+            }
+        }
+    },
+  },
+};
+
+export default resolvers;
+// 요기서 Verification 모델을 생성할 것임 -> Verification.ts로 이동
--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -31,9 +31,6 @@ class Verification extends BaseEntity{
     @Column({type:"boolean", default: false})
     used: boolean;
 
-    @ManyToOne(type => User, user => user.verifications)
-    user: User;
-
     @CreateDateColumn() createdAt: string;
     @UpdateDateColumn() updatedAt: string;
 
