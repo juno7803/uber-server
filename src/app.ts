97d0b29f8a3676a1cdb5235dd09b1d597ed08b13
junolee <juno7803@naver.com>
2020-08-02 18:47:51 +0900

    #2.45 Using Resolver Context for Authentication  Resolver에서 Context를 이용하여 객체나 함수등을 app으로부터 받을수있다.

--- src/app.ts
+++ src/app.ts
@@ -4,32 +4,47 @@ import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
 import decodeJWT from "./utils/decodeJWT";
+import {NextFunction,Response} from "express";
 
-class App{
-    public app: GraphQLServer;
-    constructor(){
-        this.app = new GraphQLServer({
-            schema: schema
-        });
-        this.middlewares();
-    }
-    // 바로 시작되는 곳
-    private middlewares = () : void => {
-        this.app.express.use(cors());
-        this.app.express.use(logger("dev"));
-        this.app.express.use(helmet());
-        this.app.express.use(this.jwt);
-    };
+class App {
+  public app: GraphQLServer;
+  constructor() {
+    this.app = new GraphQLServer({
+      schema: schema,
+      context: req=>{
+          return{
+              req: req.request
+          };
+      }
+    });
+    this.middlewares();
+  }
+  // 바로 시작되는 곳
+  private middlewares = (): void => {
+    this.app.express.use(cors());
+    this.app.express.use(logger("dev"));
+    this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
+  };
 
-    private jwt = async (req,res,next):Promise<void> => {
-        const token = req.get("X-JWT");
-        // get으로 header에서 token을 꺼내고
-        if(token){
-            const user = await decodeJWT(token);
-            // decodeJWT로 아이디가 일치한 유저를 찾음
-            console.log(user);
-        }
+  private jwt = async (
+    req,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = req.get("X-JWT");
+    // get으로 header에서 token을 꺼내고 -> X-JWT 등 지정한 name으로 꺼내옴
+    if (token) {
+      const user = await decodeJWT(token);
+      // decodeJWT로 아이디가 일치한 유저를 찾음
+      if (user) {
+        req.user = user;
+      } else {
+        req.user = undefined;
+      }
     }
+    next();
+  };
 }
 
 export default new App().app;
--- src/utils/decodeJWT.ts
+++ src/utils/decodeJWT.ts
@@ -1,12 +1,7 @@
-/* 
-    1. open token 
-    2. find userId (token안에 있던 id와 일치하는 id)
-    3. middleware에 다시 줌
-*/
 import jwt from "jsonwebtoken";
 import User from "../entities/User";
 
-const decodeJWT = async (token: string) :Promise<User|undefined>=> {
+const decodeJWT = async (token: string) :Promise<User|undefined> => {
     try{
         const decoded:any = jwt.verify(token,process.env.JWT_TOKEN || "");
         const {id} = decoded;
@@ -18,3 +13,8 @@ const decodeJWT = async (token: string) :Promise<User|undefined>=> {
 };
 
 export default decodeJWT;
+/* 
+    1. open token 
+    2. find userId (token안에 있던 id와 일치하는 id)
+    3. middleware에 다시 줌
+*/
