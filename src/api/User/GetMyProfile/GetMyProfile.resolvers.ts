bee16c4edf1f80e2af06097ad393265a940fe77a
junolee <juno7803@naver.com>
2020-08-16 16:33:04 +0900

    #2.47 Resolver Middleware를 만들어서 Resolver 보호

--- src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,16 +1,17 @@
 import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateResolver"
 
 const resolvers: Resolvers = {
-    Query:{
-        GetMyProfile: async (_,__,{req})=>{
-            const{user} = req;
-            return {
-                ok: true,
-                error: null,
-                user
-            };
-        }
-    }
+  Query: {
+    GetMyProfile: privateResolver(async (_, __, { req }) => {
+      const { user } = req;
+      return {
+        ok: true,
+        error: null,
+        user,
+      };
+    }),
+  },
 };
 
 export default resolvers;
---
+++ src/utils/privateResolver.ts
@@ -0,0 +1,17 @@
+// resolver Middleware -> resolver를 보호하기 위함
+// user를 못찾으면, Error 발생
+// user를 찾으면 resolverFunction 반환
+const privateResolver = (resolverFunction) => async (
+    parent,
+    args,
+    context,
+    info
+    ) => {
+    if(!context.req.user){
+        throw new Error("NO JWT, I refuse to proceed");
+    }
+    const resolved = await resolverFunction(parent,args,context,info);
+    return resolved;
+};
+
+export default privateResolver;
