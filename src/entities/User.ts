f1f023aa062fc38cf1d8aed2000515c39ee2723d
junolee <juno7803@naver.com>
2020-08-26 23:45:39 +0900

    0826 commit

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -87,7 +87,7 @@ class User extends BaseEntity {
   get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
-// add
+
   public comparePassword(password: string):Promise<boolean>{
     return bcrypt.compare(password,this.password);
   }
