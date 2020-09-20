d4b4e585422f67e1725d86a9207ea701cc8f5589
junholee <juno7803@naver.com>
2020-09-20 15:47:30 +0900

    #2.56.1 : BeforeUpdate() 요청되지 않는 문제 user instance save 하여 수정

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -3,30 +3,37 @@ import privateResolver from "../../../utils/privateResolver";
 import { UpdateMyProfileMutationArgs } from "../../../types/graph";
 import User from "../../../entities/User";
 
-const resolvers:Resolvers = {
-    Mutation:{
-        UpdateMyProfile: privateResolver(async(_,args:UpdateMyProfileMutationArgs,{req})=>{
-            const user: User = req.user;
-            const notNull = {}
-            Object.keys(args).forEach(key=>{
-                if(args[key] !== null){
-                    notNull[key] = args[key];
-                }
-            });
-            try{
-                await User.update({id: user.id}, {...notNull});
-                return{
-                    ok: true,
-                    error: null
-                };
-            } catch(error){
-                return{
-                    ok: false,
-                    error: error.message
-                };
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateMyProfile: privateResolver(
+      async (_, args: UpdateMyProfileMutationArgs, { req }) => {
+        const user: User = req.user;
+        const notNull = {};
+        // null이 아닌 property들만 골라내는 code (#2.56 첨고)
+        Object.keys(args).forEach((key) => {
+          if (args[key] !== null) {
+            notNull[key] = args[key];
+          }
+        });
+        try {
+            if(args.password !== null){
+                user.password = args.password;
+                user.save();
             }
-        })
-    }
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
